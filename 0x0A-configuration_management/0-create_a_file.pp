853058407e1f26b59c3a8a3addc9e1c2531664bc
sayed Elhadad <anasesozaking@gmail.com>
2024-03-22 19:02:07 +0200

    Done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Using Puppet to create a file in /tmp.
+file { '/tmp/school':
+    ensure  => 'present',
+    content => 'I love Puppet',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,2 @@
+# Project: 0x0A. Configuration management
+
