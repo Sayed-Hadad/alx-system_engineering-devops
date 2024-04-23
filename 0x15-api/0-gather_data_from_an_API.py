b77fef9cc31fd4ce00f32620df43ab96b3b29e1a
sayed elhadad <sayedelhadad@mail.com>
2024-04-24 01:54:19 +0200

    Fix pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,9 +17,9 @@ def fetch_todo_list_progress(employee_id):
     # Fetch User Data
     try:
         user_response = requests.get(user_url)
-        user_response.raise_for_status()  # Raise exception for non-200 status codes
+        user_response.raise_for_status()
         user_data = user_response.json()
-        employee_name = user_data['name']
+        e_n = user_data['name']
     except (requests.RequestException, KeyError) as e:
         print(f"Error fetching user data: {e}")
         sys.exit(1)
@@ -28,15 +28,15 @@ def fetch_todo_list_progress(employee_id):
     try:
         todos_response = requests.get(todo_url)
         todos_response.raise_for_status()
-        todo_data = todos_response.json()
+        t_data = todos_response.json()
 
         # Count the completed tasks
-        completed_tasks = sum(1 for i in todo_data if i['completed'])
+        cTasks = sum(1 for i in t_data if i['completed'])
 
         # Print progress
-        print(f'Employee {employee_name} is done with tasks({completed_tasks}/{len(todo_data)}):')
+        print(f'Employee {e_n} is done with tasks({cTasks}/{len(t_data)}):')
 
-        for task in todo_data:
+        for task in t_data:
             if task['completed']:
                 print(f'\t{task["title"]}')
     except (requests.RequestException, KeyError) as e:
