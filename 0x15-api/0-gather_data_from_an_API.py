5a7972bf1899a0668690f21d2f54415e2f06d127
sayed elhadad <sayedelhadad@mail.com>
2024-04-23 20:56:48 +0200

    Done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+def fetch_todo_list_progress(employee_id):
+    main_url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{main_url}/users/{employee_id}'
+    todo_url = f'{main_url}/todos?userId={employee_id}'
+
+    # Fetch User Data
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    employee_name = user_data['name']
+
+    # Fetch User Data
+    todos_response = requests.get(todo_url)
+    todo_data = todos_response.json()
+
+    # Count the completed tasks
+    total_task = len(todo_data)
+    completed_task = []
+    for i in todo_data:
+        if i['completed']:
+            completed_task.append(1)
+            
+    # Print progress
+    print(f'Employee {employee_name} is done with tasks({len(completed_task)}/{total_task}):')
+    for task in todo_data:
+        if task['completed']:
+            print(f'\t{task["title"]}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 script.py <employee_id>')
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    fetch_todo_list_progress(employee_id)
---
+++ 0x15-api/README.md
@@ -0,0 +1,5 @@
+# 0x15. API
+
+0. **Gather data from an API**: This project task has been completed. The code for gathering data from an API can be found in the following file:
+
+    - [0-gather_data_from_an_API.py](0-gather_data_from_an_API.py)
