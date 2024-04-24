926ac07b8894b72f40a0f594a786f61811d33f95
sayed elhadad <sayedelhadad@mail.com>
2024-04-24 12:58:47 +0200

    Done

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+Python script to export data in the CSV format.
+'''
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    users_url = url+f'users/{argv[1]}'
+    todo_url = url+'todos'
+    user = requests.get(users_url).json()
+    todo_lis = requests.get(todo_url, params={'userId': argv[1]}).json()
+    with open(f"{argv[1]}.csv", "w") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for line in todo_lis:
+            writer.writerow([argv[1], user.get('username'),
+                            line.get('completed'), line.get('title')])
+        
