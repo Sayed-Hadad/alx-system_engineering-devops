1e3c0d3ec39291daaeb3ff4c1165516550dc6af4
sayed elhadad <sayedelhadad@mail.com>
2024-04-24 02:02:48 +0200

    Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,58 +1,24 @@
 #!/usr/bin/python3
 
 '''
-A Python script that, using this REST API,
-for a given employee ID, returns information
-about his/her TODO list progress.
+ a Python script that, using this REST API,
+ for a given employee ID, returns information
+ about his/her TODO list progress.
 '''
 import requests
-import sys
-
-
-def fetch_todo_list_progress(employee_id):
-    main_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{main_url}/users/{employee_id}'
-    todo_url = f'{main_url}/todos?userId={employee_id}'
-
-    # Fetch User Data
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        user_data = user_response.json()
-        e_n = user_data['name']
-    except (requests.RequestException, KeyError) as e:
-        print(f"Error fetching user data: {e}")
-        sys.exit(1)
-
-    # Fetch Todo Data
-    try:
-        todos_response = requests.get(todo_url)
-        todos_response.raise_for_status()
-        t_data = todos_response.json()
-
-        # Count the completed tasks
-        cTasks = sum(1 for i in t_data if i['completed'])
-
-        # Print progress
-        print(f'Employee {e_n} is done with tasks({cTasks}/{len(t_data)}):')
-
-        for task in t_data:
-            if task['completed']:
-                print(f'\t{task["title"]}')
-    except (requests.RequestException, KeyError) as e:
-        print(f"Error fetching todo data: {e}")
-        sys.exit(1)
-
+from sys import argv
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: python3 script.py <employee_id>')
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-
-    fetch_todo_list_progress(employee_id)
+    url = 'https://jsonplaceholder.typicode.com/'
+    users_url = url+f'users/{argv[1]}'
+    todo_url = url+'todos'
+    user = requests.get(users_url).json()
+    todo_lis = requests.get(todo_url, params={'userId': argv[1]}).json()
+    todo_completed = []
+    for task in todo_lis:
+        if task.get('completed') is True:
+            todo_completed.append(task.get('title'))
+    print(
+        f"Employee {user.get('name')}\
+ is done with tasks({len(todo_completed)}/{len(todo_lis)}):")
+    [print(f"\t {task}") for task in todo_completed]
