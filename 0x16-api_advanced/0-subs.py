ee3e733f7a5f64830ca54895687ee62b2e1cdddd
sayed__elhadad <sayedelhadad@gmail.com>
2024-05-08 02:50:05 +0300

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python3
 import requests
 
-
 """
-Fetch number_of_subscribers From
-Reddit Api
+This module provides a function to fetch the number of subscribers from the Reddit API.
 """
-
 
 def number_of_subscribers(subreddit):
+    """
+    Fetches the number of subscribers for a given subreddit from the Reddit API.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The total number of subscribers for the given subreddit. Returns 0 if the subreddit is invalid or if an error occurs.
+
+    Raises:
+        None
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyAPI/1.0.0'}
     response = requests.get(url, headers=headers)
