6daac4879ebb409dfedfb8e3a623b85b3ece4553
sayed elhadad <sayedelhadad@mail.com>
2024-05-08 03:14:37 +0300

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""
-Count the number of subscribers to a subreddit.
-"""
-import requests
+"""Module for task 0"""
+
 
 def number_of_subscribers(subreddit):
-    """
-    Return the total number of subscribers for the given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-        
-    Returns:
-        int: The total number of subscribers for the given subreddit.
-             Returns 0 if the subreddit is invalid or if an error occurs.
-    """
-    reddit_api_url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyAPI/1.0.0'}
-    response = requests.get(reddit_api_url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    
-    return 0
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
