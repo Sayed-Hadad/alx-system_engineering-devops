1a0316868fa7ba932b7a5f9472519ef774c67d85
sayed__elhadad <sayedelhadad@gmail.com>
2024-05-09 13:02:31 +0300

    Done!

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,7 +15,7 @@ def top_ten(subreddit):
     titles = []
     endpoint = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-Agent': 'ApiTraining'}
-    response = requests.get(endpoint, headers=headers)
+    response = requests.get(endpoint, headers=headers , allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         for post in data.get('data', {}).get('children', []):
